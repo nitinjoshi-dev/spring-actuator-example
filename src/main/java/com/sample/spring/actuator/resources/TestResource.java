aad23982aad7c746589ff1ba65f362a9566d4308
Nitin Joshi <nitinjsh10@gmail.com>
2020-05-02 22:41:46 +0530

    [CHANGES] to get log file

--- src/main/java/com/sample/spring/actuator/resources/TestResource.java
+++ src/main/java/com/sample/spring/actuator/resources/TestResource.java
@@ -2,6 +2,8 @@ package com.sample.spring.actuator.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -20,12 +22,16 @@ import io.micrometer.core.annotation.Timed;
 @RequestMapping("/api")
 public class TestResource {
 	
+	private static final Logger log = LoggerFactory.getLogger(TestResource.class);
+	
 	@Autowired
 	private PizzaService pizzaService;
 
 	@GetMapping(path = "/pizzas")
 	public ResponseEntity<List<PizzaDTO>> getAllPizza(@RequestParam(name = "size", required = false) Integer size) {
+		log.info("inside getAllPizza {}", size);
 		List<PizzaDTO> pizzaDTOs = pizzaService.getAllPizzas(size);
+		log.info("Exiting getAllPizza");
 		return new ResponseEntity<List<PizzaDTO>>(pizzaDTOs, HttpStatus.OK);
 	}
 
