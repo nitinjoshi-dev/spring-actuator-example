f9bcbf94061522ee3576e4e5d6a50f69e08ff9cc
rajatraghavendran <rajat.raghavendran@neebal.com>
2020-05-03 01:32:30 +0530

    Jhipster custom configuratoin of metrics

--- src/main/java/com/sample/spring/actuator/ActuatorApplication.java
+++ src/main/java/com/sample/spring/actuator/ActuatorApplication.java
@@ -1,14 +1,48 @@
 package com.sample.spring.actuator;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 
 @SpringBootApplication
 public class ActuatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActuatorApplication.class, args);
+	}
+	
+	@Bean
+	public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+	    return registry -> registry.config()
+	        .commonTags("myTag", "myTagValue")
+	        .meterFilter(new MeterFilter() {
+	
+	            @Override
+	            public DistributionStatisticConfig configure(Meter.Id id,
+	                                                         DistributionStatisticConfig config) {
+	                if (id.getName().startsWith("http.server.requests")) {
+	                    return config.merge(DistributionStatisticConfig.builder()
+	                        .percentilesHistogram(true)
+	                        .percentiles(0.5, 0.9, 0.99)
+	                        .percentilePrecision(1)
+	                        .minimumExpectedValue(1L)
+	                        .maximumExpectedValue(Long.MAX_VALUE)
+	                        .expiry(Duration.ofHours(1))
+	                        .bufferLength(2)
+	                        .build());
+	                }
+	                return config;
+	            }
+	        });
 	}
 
 }
